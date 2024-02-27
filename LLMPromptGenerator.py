8ac30358dc2ad79391b02ab7c09e4df53d68e021
Grant Lewis <grant@lewisfamily.org>
2024-02-27 14:47:01 -0700

    modified extractor to support prompt eng

--- LLMPromptGenerator.py
+++ LLMPromptGenerator.py
@@ -52,18 +52,14 @@ class MusicGenInfo(BaseModel):
         
 class LLMPromptGenerator:
     def __init__(self, model_name="TheBloke/Llama-2-7b-Chat-GPTQ"): # device="cpu"
-        # self.device = device
         self.model_name = model_name
         self.hf_pipeline = pipeline('text-generation', model=model_name, device_map='auto') # device=device)
-        # self.jsons = []
         self.info = []
         self.long_term_prompt = None
         
     def generate_prompt(self, text, music_gen_info=MusicGenInfo):
-        
         if isinstance(text, list):
             return [self.generate_prompt(t, music_gen_info) for t in text]
-        # print(music_gen_info.schema_json())
         
         prompt = f"""
         {text}\n\n In the following format {music_gen_info.schema_json()}, A piece music generated as background ambience for the above text would have these qualities: \n
@@ -75,11 +71,11 @@ class LLMPromptGenerator:
         parser = JsonSchemaParser(music_gen_info.schema())
         prefix_function = build_transformers_prefix_allowed_tokens_fn(self.hf_pipeline.tokenizer, parser)
         output_dict = self.hf_pipeline(prompts, prefix_allowed_tokens_fn=prefix_function)
-        # info = MusicGenInfo(**output_dict[0]['generated_text'])
-        if flush:
-            # self.jsons = []
+        
+        if flush: # Empties the info list and long_term_prompt
             self.info = []
             self.long_term_prompt = None
+            
         results = []
         if isinstance(text, list):
             for i, p in enumerate(prompts):
@@ -91,12 +87,10 @@ class LLMPromptGenerator:
         return result
 
     def prompts(self):
-        
         if self.long_term_prompt is None:
             self.long_term_prompt = str(self.info[0].long_term)
         
         prompts = []
-        
         for i in self.info:
             prompts.append(f"{self.long_term_prompt} {str(i.short_term)}")
         
